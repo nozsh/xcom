b1b5a9f56198652556a374c32ec576a17c04194e
nozsh <155148075+nozsh@users.noreply.github.com>
2024-06-02 02:42:15 +0300

    upd

--- ai/aicfg.js
+++ ai/aicfg.js
@@ -2,8 +2,7 @@ let chat_Chat_a = "https://niansuhai-llms8.hf.space/"; // /chat/a/
 let chat_ChatGpt = "https://gpt-chatbotru-chat1.ru/#/chat"; // /chat/gpt
 let chat_Gpt35Turbo = "https://chatgptbots-2.hf.space/"; // /chat/gpt35turbo/
 let chat_Bing = "https://chatgptbots-ai.hf.space/"; // /chat/bing/
-let chat_HfJb =
-  "https://huggingface.co/chat/conversation/665b9bd76419a06e31264573"; // /chat/huggingface-jailbreak/
+let chat_HfJb = "https://hf.co/chat/assistant/6620df7598f70fdf437ad579"; // /chat/huggingface-jailbreak/
 
 let image_Gen_a = "https://chatgptbots-image-gen-pro.hf.space/"; // /image-gen/a/
 
