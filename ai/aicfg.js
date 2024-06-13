813a9f3bf7132feacaaf776115e5272184e8a87a
nozsh <155148075+nozsh@users.noreply.github.com>
2024-06-13 16:56:59 +0300

    upd ai/gpt

--- ai/aicfg.js
+++ ai/aicfg.js
@@ -1,5 +1,5 @@
 let chat_Chat_a = "https://niansuhai-llms8.hf.space/"; // /chat/a/
-let chat_ChatGpt = "https://gpt-chatbotru-chat1.ru/#/chat"; // /chat/gpt
+let chat_ChatGpt = "https://gpt-chatbotru-chat2-next.ru/#/chat"; // /chat/gpt
 let chat_Gpt35Turbo = "https://chatgptbots-2.hf.space/"; // /chat/gpt35turbo/
 let chat_Bing = "https://chatgptbots-ai.hf.space/"; // /chat/bing/
 let chat_HfJb = "https://hf.co/chat/assistant/6620df7598f70fdf437ad579"; // /chat/huggingface-jailbreak/
